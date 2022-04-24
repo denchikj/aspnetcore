df559689eb91b1a7096effe2fa11e3646f459c8d
denchikj <igoshevdenis9876@gmail.com>
2022-04-24 19:00:44 +0500

    Transition from minimal api to controllers

--- Minimal.WebApi/Program.cs
+++ Minimal.WebApi/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -13,53 +13,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var meetups = new List<Meetup>();
-
-app.MapGet("/meetups", () => Results.Ok(meetups));
-
-app.MapDelete("/meetups/{id:guid}", ([FromRoute] Guid id) =>
-{
-    var meetupToDelete = meetups.SingleOrDefault(meetup => meetup.Id == id);
-
-    if (meetupToDelete is null)
-    {
-        return Results.NotFound();
-    }
-
-    meetups.Remove(meetupToDelete);
-    return Results.Ok(meetupToDelete);
-});
-
-app.MapPost("/meetups", ([FromBody] Meetup newMeetup) =>
-{
-    newMeetup.Id = Guid.NewGuid();
-    meetups.Add(newMeetup);
-
-    return Results.Ok(newMeetup);
-});
-
-app.MapPut("/meetups/{id:guid}", ([FromRoute] Guid id, [FromBody] Meetup updatedMeetup) =>
-{
-    var oldMeetup = meetups.SingleOrDefault(meetup => meetup.Id == id);
-
-    if (oldMeetup is null)
-    {
-        return Results.NotFound();
-    }
-
-    oldMeetup.Topic = updatedMeetup.Topic;
-    oldMeetup.Place = updatedMeetup.Place;
-    oldMeetup.Duration = updatedMeetup.Duration;
-
-    return Results.NoContent();
-});
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
 
-class Meetup
-{
-    public Guid? Id { get; set; }
-    public string Topic { get; set; }
-    public string Place { get; set; }
-    public int Duration { get; set; }
-}
