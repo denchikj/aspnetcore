165e87729c2a460f3e4d8bdfcd431b8ab7504193
denchikj <igoshevdenis9876@gmail.com>
2022-04-24 19:18:31 +0500

    add Dto

---
+++ Minimal.WebApi/Meetup/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace Minimal.WebApi.Meetup;
+
+internal class Meetup
+{
+    public Guid Id { get; set; }
+    public string Topic { get; set; }
+    public string Place { get; set; }
+    public int Duration { get; set; }
+}
+
