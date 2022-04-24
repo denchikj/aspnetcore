b50ce17ddefb8e4c923e5a9723522907d59e5bc8
denchikj <igoshevdenis9876@gmail.com>
2022-04-24 19:40:19 +0500

    documented dto and controllers

--- Minimal.WebApi/Meetup/Controller.cs
+++ Minimal.WebApi/Meetup/Controller.cs
@@ -1,13 +1,21 @@
 ﻿namespace Minimal.WebApi.Meetup;
 
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 [ApiController]
 [Route("/meetups")]
-public class Controller : ControllerBase
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
+public class MeetupController : ControllerBase
 {
     private static readonly ICollection<Meetup> Meetups = new List<Meetup>();
 
+    /// <summary>
+    /// Create a new meetup
+    /// </summary>
+    /// <param name="createDto">Meetup creation information</param>
+    /// <response code="200">Newly created meetup</response>
     [HttpPost]
     public IActionResult CreateMeetup([FromBody] CreateMeetupDto createDto)
     {
@@ -31,6 +39,8 @@ public class Controller : ControllerBase
         return Ok(readDto);
     }
 
+    /// <summary>Retrieve all meetups.</summary>
+    /// <response code="200">All meetups.</response>
     [HttpGet]
     public IActionResult GetAllMeetups()
     {
@@ -45,8 +55,14 @@ public class Controller : ControllerBase
         return Ok(readDtos);
     }
 
-
+    /// <summary>Update meetup with matching id.</summary>
+    /// <param name="id" example="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx">Meetup id.</param>
+    /// <param name="updateDto">Meetup update information.</param>
+    /// <response code="204">Meetup was updated successfully.</response>
+    /// <response code="404">Meetup with specified id was not found.</response>
     [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] UpdateMeetupDto updateDto)
     {
         var oldMeetup = Meetups.SingleOrDefault(meetup => meetup.Id == id);
@@ -63,7 +79,13 @@ public class Controller : ControllerBase
         return NoContent();
     }
 
+    /// <summary>Delete meetup with matching id.</summary>
+    /// <param name="id" example="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx">Meetup id.</param>
+    /// <response code="200">Deleted meetup.</response>
+    /// <response code="404">Meetup with specified id was not found.</response>
     [HttpDelete("{id:guid}")]
+    [ProducesResponseType(typeof(ReadMeetupDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult DeleteMeetup([FromRoute] Guid id)
     {
         var meetupToDelete = Meetups.SingleOrDefault(meetup => meetup.Id == id);
